0b05ce13b70516253442a14f5fe0d743f6be4414
Alex Campulungeanu <allecs1987@yahoo.com>
2021-05-15 00:53:13 +0300

    reformat to questions

--- src/helpers/questions.js
+++ src/helpers/questions.js
@@ -16,6 +16,30 @@ const askProjectType = async () => {
   return type
 }
 
+const askWhatFilesToCopy = async (choices) => {
+  const files = await inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Select files to copy',
+        name: 'files',
+        pageSize: choices.length,
+        choices: 
+          // new inquirer.Separator(' = The Files = '),
+          choices
+        ,
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return 'You must choose at least one file.';
+          }
+          return true;
+        },
+      },
+    ])
+  return files
+}
+
 module.exports = {
-    askProjectType
+    askProjectType,
+    askWhatFilesToCopy
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 
-const inquirer = require('inquirer')
 const fs = require('fs')
 const path = require('path')
 
@@ -8,7 +7,8 @@ const questions = require('./helpers/questions')
 const filesHelper = require('./helpers/files')
 const appConstants = require('./config/constants')
 
-questions.askProjectType().then((type) => {
+const main = async () =>{
+  const type = await questions.askProjectType()
   const templateFiles = filesHelper.getFilesFromTemplate(type.projectType)
   const choices = []
   templateFiles.forEach((file) => {
@@ -25,33 +25,19 @@ questions.askProjectType().then((type) => {
       })
     }
   })
+
   // ask what files should be copied
-  inquirer
-  .prompt([
-    {
-      type: 'checkbox',
-      message: 'Select files to copy',
-      name: 'files',
-      pageSize: choices.length,
-      choices: 
-        // new inquirer.Separator(' = The Files = '),
-        choices
-      ,
-      validate: function (answer) {
-        if (answer.length < 1) {
-          return 'You must choose at least one file.';
-        }
-        return true;
-      },
-    },
-  ])
-  .then((answers) => {
-    answers.files.forEach(file => {
-      // copy file from package module tu current directory where package is called
-      const filePath = path.join(__dirname, appConstants.templatesDir, type.projectType, file);
-      const newPath = path.join(path.join(filesHelper.getCurrentDirectory(), file))
-      // fs.copyFileSync(filePath, newPath)
-    })
-    console.log('FILE copied !');
-  });
-})
+  const filesToCopy = await questions.askWhatFilesToCopy(choices)
+  
+  // copy files from package module tu current directory where package is called
+  filesToCopy.files.forEach(file => {
+    const filePath = path.join(__dirname, appConstants.templatesDir, type.projectType, file);
+    const newPath = path.join(path.join(filesHelper.getCurrentDirectory(), file))
+    fs.copyFileSync(filePath, newPath)
+  })
+}
+
+main()
+  .then(r =>  console.log('FILE copied !'))
+  .catch(err => console.log('Something happend !', err))
+
